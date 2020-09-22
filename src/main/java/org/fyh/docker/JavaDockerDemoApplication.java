e24ae3b5741b7ac68ed21bde19b4e4a732a1970f
fangyunhe <842241090>
2020-09-22 11:45:02 +0800

    init

---
+++ src/main/java/org/fyh/docker/JavaDockerDemoApplication.java
@@ -0,0 +1,13 @@
+package org.fyh.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDockerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaDockerDemoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/fyh/docker/controller/TestController.java
@@ -0,0 +1,41 @@
+package org.fyh.docker.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author fangyunhe
+ * @version 1.0
+ * @Description
+ * @time 2020/9/21 16:26
+ */
+@Slf4j
+@RestController
+@RequestMapping("test")
+public class TestController {
+
+    @RequestMapping("one")
+    public Map<String, String> one(){
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("one", "1");
+        hashMap.put("two", "2");
+        hashMap.put("three", "3");
+        hashMap.put("timestamp", String.valueOf(System.currentTimeMillis()));
+        log.info("hashMap={}", hashMap);
+        return hashMap;
+    }
+
+    @RequestMapping("two")
+    public Map<String, String> two(){
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("1", "1");
+        hashMap.put("2", "2");
+        hashMap.put("timestamp", String.valueOf(System.currentTimeMillis()));
+        log.error("hashMap={}", hashMap);
+        return hashMap;
+    }
+}
